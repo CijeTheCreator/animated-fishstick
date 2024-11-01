5c174ca799db5cd72b5b2179a504da40e715b23f
Chijioke <osadebec98@gmail.com>
2024-11-01 20:00:00 +0100

    Added Snippets for Prisma, Zod

--- nvim/lua/custom/snippets/prisma.lua
+++ nvim/lua/custom/snippets/prisma.lua
@@ -1,7 +1,13 @@
 local luasnip = require("luasnip")
-
+-- set keybinds for both INSERT and VISUAL.
+vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>luasnip-next-choice", {})
+vim.api.nvim_set_keymap("s", "<C-n>", "<Plug>luasnip-next-choice", {})
+vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>luasnip-prev-choice", {})
+vim.api.nvim_set_keymap("s", "<C-p>", "<Plug>luasnip-prev-choice", {})
+local fmt = require("luasnip.extras.fmt").fmt
 local s = luasnip.snippet
 local t = luasnip.text_node
+local c = luasnip.choice_node
 local i = luasnip.insert_node
 luasnip.add_snippets("typescript", {
 s("pff", {
@@ -62,4 +68,74 @@ s("pu", { --prisma update
 }),
 })
 
-
+--TODO: Add String Ids later
+luasnip.add_snippets("prisma", {
+  s("pm", fmt([[
+  model {} {{
+    id         Int        @id @default(autoincrement())
+    createdAt  DateTime   @default(now())
+    updatedAt  DateTime   @updatedAt
+    {}
+    }}
+  ]], {
+    i(1, ""),i(2, ""),
+  })),
+  s("pfi", fmt([[
+  {}          {}{}      {}
+  ]], {
+      i(1, ""),
+      c(2, {
+        t("Int"),
+        t("String"),
+        t("Boolean"),
+        t("BigInt"),
+        t("Float"),
+        t("DateTime"),
+        t("Json"),
+        t("Bytes"),
+        t("Unsupported"),
+        i(nil, "Custom"),
+      }),
+      c(3, {
+        t(""),
+        t("[]"),
+        t("?"),
+        t("[]?"),
+      }),
+      c(4, {
+        t(""),
+        t("@id"),
+        t("@@id"),
+        t("@default"),
+        t("@unique"),
+        t("@@unique"),
+        t("@@index"),
+        t("@relation"),
+        t("@map"),
+        t("@@map"),
+        t("@updatedAt"),
+        t("@ignore"),
+        t("@@ignore"),
+        t("@@schema"),
+      }),
+  })),
+  s("pattr", fmt([[
+  {}
+  ]], {
+    c(1, {
+      t("@relation"),
+      t("@unique"),
+      t("@@unique"),
+      t("@id"),
+      t("@@id"),
+      t("@default"),
+      t("@@index"),
+      t("@map"),
+      t("@@map"),
+      t("@updatedAt"),
+      t("@ignore"),
+      t("@@ignore"),
+      t("@@schema"),
+    }),
+      })),
+    })
